ebcc1e73b9f794e2b1450ed503548b8aca88bb5b
NguyenTrung2002 <NguyenTrung2002>
2023-11-09 22:36:37 +0700

    delete thutu_danhmuc

--- admincp/modules/quanlydanhmucsanpham/xuly.php
+++ admincp/modules/quanlydanhmucsanpham/xuly.php
@@ -1,13 +1,12 @@
 <?php
 include('../../config/config.php');
     $tenloaisp = $_POST['tendanhmuc'];
-    $thutu = $_POST['thutu'];
     if(isset($_POST['themdanhmucsanpham'])){
-        $sql_them = "INSERT INTO tbl_danhmuc(ten_danhmuc, thutu_danhmuc) VALUE ('".$tenloaisp."','".$thutu."' )";
+        $sql_them = "INSERT INTO tbl_danhmuc(ten_danhmuc) VALUE ('".$tenloaisp."')";
         mysqli_query($conn, $sql_them);
         header('Location:../../index.php?action=quanlydanhmucsanpham&query=them');
     }elseif(isset($_POST['suadanhmuc'])){
-        $sql_update = "UPDATE tbl_danhmuc SET ten_danhmuc = '".$tenloaisp."', thutu_danhmuc = '".$thutu."' WHERE id_danhmuc = '$_GET[id_danhmuc]'";
+        $sql_update = "UPDATE tbl_danhmuc SET ten_danhmuc = '".$tenloaisp."' WHERE id_danhmuc = '$_GET[id_danhmuc]'";
         mysqli_query($conn, $sql_update);
         header('Location:../../index.php?action=quanlydanhmucsanpham&query=them');
     }else{
